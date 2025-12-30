51a7c1489ef868ffe5aadf8a1f1e7fcd921758e1
Rajin <Rajin.Zaman@gmail.com>
2025-12-29 19:42:23 -0500

    Implemented SDL Window

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,43 @@ using namespace std;
 
 int main()
 {
+    const int WIN_W = 800;  // Window WIDTH
+    const int WIN_H = 600;  // Window HEIGHT
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         cerr << "SDL initialization failed." << endl;
-        return 1; // ERROR GETTING SDL
+        return 1;  // Error getting SDL.
     }
 
-    cout << "Success!" << endl;
+    SDL_Window *window = SDL_CreateWindow(
+            "Particle Fire Explosion", SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED, WIN_W, WIN_H, SDL_WINDOW_SHOWN);
+    
+    if (window == NULL) {
+        cerr << "Could not create window: " << SDL_GetError() << endl;
+        SDL_Quit();
+        return 2;  // Error creating an SDL window.
+    }
 
-    SDL_Quit();
+
+    bool quit = false;
+    SDL_Event event;
+
+    while (!quit) {
+        // Update particles
+        // Draw particles
+        // Check for messages/events
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) { quit = true; }
+        }
+    }
+
+    cout << "Closing window ..." << endl;
+    SDL_DestroyWindow(window);
+    
+    cout << "Closing program ..." << endl;
+    SDL_Quit();  // Close SDL connection
+
+    return 0;  // End of program.
 }
